81d954179f3677da43e13a54800aaba68d40f4a9
bill <371570273@qq.com>
2018-10-31 14:15:52 +1030

    update best model

--- run_mnasnet.sh
+++ run_mnasnet.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-python wh_mnasnet_train.py --epochs 1000 --resume ./top1_68.3%_model_best.pth.tar ./ilsvrc12/
+python wh_mnasnet_train.py --epochs 1000 --resume ./model_best.pth.tar ./ilsvrc12/
